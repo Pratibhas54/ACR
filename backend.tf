f453e3e833e0c6843841d44b9cbd827d8bf5c4b9
Pratibhas54 <112929894+Pratibhas54@users.noreply.github.com>
2024-10-02 23:21:55 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-backend"
-    storage_account_name = "tfstateaccount"
+    storage_account_name = "tfstateaccount12"
     container_name       = "tfstate"
     key                  = "acr.terraform.tfstate"
   }
